feeee827481de41174f499ee37391246ea21bd48
denysbv <borysenko.d@gmail.com>
2020-04-07 14:26:13 +0300

    updated

--- src/com/SoftServAcademy/Main.java
+++ src/com/SoftServAcademy/Main.java
@@ -1,30 +1,39 @@
 package com.SoftServAcademy;
 
-import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
 
+    public static String createName (String name) {
+        if (nameValidation(name)) {
+			System.out.println(name + " name created");
+        	return name;
+        } else {
+			System.out.println("Invalid name. The user name can be 3 to 15 characters of the Latin alphabet, numbers, and underscores.");
+			return null;
+		}
+    }
+
+    private static boolean nameValidation (String name) {
+        Pattern pattern = Pattern.compile("([a-zA-Z0-9_]{3,15})");
+        Matcher matcher = pattern.matcher(name);
+        if (matcher.matches()) {
+            return true;
+        } else return false;
+    }
+
     public static void main(String[] args) {
     	// #1
-	String one = "House";
-	String two = "Full house of House is full";
-	boolean b = two.contains(one);
-	System.out.println(b);
+		String one = "House";
+		String two = "Full house of House is full";
+		boolean b = two.contains(one);
+		System.out.println(b);
 
-	// #2
+		// #2
 		String name = "Bob";
 		String surname = "Smith";
 		String patronymic = "Andreyevich";
-		Scanner scanner = new Scanner(System.in);
-//		System.out.println("Enter name: ");
-//		name = scanner.nextLine();
-//		System.out.println("Enter surname: ");
-//		surname = scanner.nextLine();
-//		System.out.println("Enter patronymic: ");
-//		patronymic = scanner.nextLine();
-
 		StringBuilder sb = new StringBuilder();
 		System.out.println(sb.append(surname).append(" ").append(name.charAt(0)).append(".")
 				.append(patronymic.charAt(0)).append("."));
@@ -33,19 +42,10 @@ public class Main {
 		System.out.println(sb.append(name).append(", ").append(patronymic).append(" ").append(surname));
 
 		// #3
-		String name2;
-		System.out.println("Enter name: ");
-		Pattern pattern = Pattern.compile("([a-zA-Z0-9_]{3,15})");
-		String temp = scanner.next();
-		sb.delete(0, sb.capacity());
-		Matcher matcher = pattern.matcher(temp);
-		if (matcher.matches()) {
-			sb.append(temp);
-			name2 = sb.toString();
-			System.out.println(name2);
-		} else System.out.println("Invalid name. The user name can be 3 to 15 characters of the Latin alphabet, numbers, and underscores.");
-
-
-		scanner.close();
+		createName("bob_2");
+		createName("bo");
+		createName("bob_2gdsfsdfsdf");
+		createName("bob_3");
+		createName("bob_6");
     }
 }
